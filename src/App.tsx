f5fb95e0484d6db655877b2420f94307afbd00b0
GGTTGTGt <zz814170@gmail.com>
2025-08-02 17:46:15 +0700

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,41 @@
   import type { TaskCardProps } from "./libs/types";
 
   function App() {
-    const Taskcards: TaskCardProps[] = [
-      {id: 1,title: "Read a book",description: "Vite + React + Bootstrap + TS",isDone: false},
-      {id: 2,title: "Write a code",description: "Finish project for class",isDone: false},
-      {id: 3,title: "Deploy app",description: "Push project to Github Pages",isDone: false}
-    ]
-    return (
-      <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
-        {/* Header ด้านบน*/}
-        <Header></Header>
-        <div className="d-flex flex-grow-1">
-          {/* Sidebar ด้านซ้าย */}
-          <Sidebar userName="worachai" type="admin"></Sidebar>
-          {/* Main content */}
-          <div className="container text-center">
-            <main className="flex-grow-1 p-4">
-              <div className="col-12 m-2 p-0">
-              {/* Card รายการ */}
-              <TaskInput></TaskInput>
-              <TaskCard {...Taskcards[0]}/>
-              <TaskCard {...Taskcards[1]}/>
-              <TaskCard {...Taskcards[3]}/>
-              </div>
-            </main>
-          </div>
+  const TaskCards: TaskCardProps[] = [
+    {id: 1, title: "Read a book", description: "Vite + React + Bootstrap + TS", isDone: false},
+    {id: 2, title: "Write code", description: "Finish project for class", isDone: false},
+    {id: 3, title: "Deploy app", description: "Push project to GitHub Pages", isDone: false},
+];
+
+  return (
+    <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
+      {/* Header ด้านบน */}
+      <Header></Header>
+
+      <div className="d-flex flex-grow-1">
+        {/* Sidebar ด้านซ้าย */}
+        
+
+        <Sidebar userName = "worachai" type = "student"></Sidebar>
+        {/* Main content */}
+        <div className="container text-center">
+          <main className="flex-grow-1 p-4">
+            {/* input ด้านบน*/}
+            <TaskInput></TaskInput>
+
+
+            <TaskCard { ...TaskCards[0] } />
+            <TaskCard { ...TaskCards[1] }></TaskCard>
+            <TaskCard { ...TaskCards[2] }></TaskCard>
+
+          </main>
         </div>
-
-        <Footer year="2025" fullName="Worachai Thaweetanamongkol" studentId="670610728"></Footer>
       </div>
-    );
-  }
 
-  export default App;
+      {/* Footer ด้านล่าง */}
+    <Footer year ="2026" fullName = "Worachai Thaweetanamongkol" studentId = "670610728"></Footer>
+    </div>
+  );
+}
+
+export default App;
